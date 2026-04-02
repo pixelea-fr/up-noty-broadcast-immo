bbf406d5868cc4a2d14eba86e4a1d14dd84971c3
pixelea-fr <equipe@pixelea.fr>
2026-04-02 17:14:41 +0200

    ajustement taille des cards

--- templates/card.php
+++ templates/card.php
@@ -1,18 +1,14 @@
 <?php
-
 if (! isset($post_id)) {
     return;
 }
-
 $post_id = (int) $post_id;
 
 $annonce = (isset($annonce) && is_object($annonce)) ? $annonce : null;
 if (! $annonce || ! isset($annonce->bien) || ! is_object($annonce->bien)) {
     return;
 }
-
 $bien = $annonce->bien;
-
 ?>
 <div class="up-immo-card" data-postid="<?php echo $post_id; ?>" data-templates="card">
     <?php if (has_post_thumbnail($post_id)) : ?>
@@ -41,17 +37,17 @@ $bien = $annonce->bien;
         <div class="up-immo-card__price-container"><span class="up-immo-card__price">
                 <?php echo $bien->prix_ou_loyer !== '' ? esc_html($bien->prix_ou_loyer) : 'Prix sur demande'; ?>
             </span>
-            <?php if ($bien->loyer !== '' && $bien->loyer_periodicite !== '') : ?>
+            <!-- <?php if ($bien->loyer !== '' && $bien->loyer_periodicite !== '') : ?>
                 <span><?php echo esc_html(' (' . $bien->loyer_periodicite . ')'); ?></span>
-            <?php endif; ?>
+            <?php endif; ?> -->
         </div>
         <h3 class="up-immo-card__title">
             <a class="up-immo-card__title-link" href="<?php echo esc_url($annonce->lien); ?>"><?php echo esc_html($annonce->titre); ?></a>
         </h3>
-        <div class="p-immo-card__details">
+        <div class="up-immo-card__details">
         <?php
        foreach ( $annonce->bien->resume_details as $detail ) {
-    echo '<div class="p-immo-card__detail">';
+    echo '<div class="up-immo-card__detail">';
     echo '<span class="label">' . esc_html( $detail['label'] ) . ' :</span> ';
     echo '<span class="value">' . esc_html( $detail['value'] ) . '</span>';
     echo '</div>';
